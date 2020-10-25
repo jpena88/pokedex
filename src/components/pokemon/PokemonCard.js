acc9ec44a6d78edc11f978df29fc3375ebe57ab9
John Peña <john.pena@udemy.com>
2020-10-24 19:31:05 -0700

    Add pokemon sprites to page
    Signed-off-by: John Peña <john.pena@udemy.com>

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 
+import styled from 'styled-components';
+
+const Sprite = styled.img`
+  width: 5em;
+  height: 5em;
+`;
+
 export default class PokemonCard extends Component {
 state = {
   name: '',
   imageUrl: '',
   pokemonIndex: ''
 };
+
+componentDidMount () {
+  const { name, url } = this.props;
+  const pokemonIndex = url.split("/")[url.split("/").length - 2];
+  const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`
+
+  this.setState({
+    name,
+    imageUrl,
+    pokemonIndex,
+  });
+}
 
   render() {
     const name = this.props.name;
@@ -14,8 +33,23 @@ state = {
     return (
       <div className='col-md-3 col-sm-6 mb-5'>
         <div className="card">
-          <div className="card-header">
-            <h1>{name}</h1>
+          <h5 className="card-header">{this.state.pokemonIndex}</h5>
+          <Sprite
+            className="card-img-top rounded mx-auto mt-2" 
+            // onLoad={() => this.setState({ imageLoading: false })}
+            // onError={() => this.setState({ tooManyRequests: true })}
+            src={this.state.imageUrl}
+          />
+          <div className="card-body mx-auto">
+            <h6 className="card-title">
+              {this.state.name
+                .toLowerCase()
+                .split(" ")
+                .map(
+                  letter => letter.charAt(0).toUpperCase() + letter.substring(1)
+                )
+                .join(' ')}
+            </h6>
           </div>
         </div>
       </div>
